ddb885e88cc44fc92eed9db2634a0186265f3a3e
Ian Ross <ianhowesross@gmail.com>
2019-04-10 17:06:22 -0400

    Setting up board

--- smacktalktoe.py
+++ smacktalktoe.py
@@ -1,4 +1,4 @@
-print("Hello, Smack Talk Toe, user!")
+# Smack Talk Toe
 
 """
 --Psuedo code--
@@ -12,3 +12,25 @@ Initial Tasks:
 -Allow game to be played again
 """
 
+class Board(object):
+    # Initializes list of 9 cells
+    def __init__(self):
+        self.cells = [" "] * 9
+
+    # Display the board
+    def display(self):
+        print(self.cells[0], "|", self.cells[1], "|", self.cells[2])
+        print("---------")
+        print(self.cells[3], "|", self.cells[4], "|", self.cells[5])
+        print("---------")
+        print(self.cells[6], "|", self.cells[7], "|", self.cells[8])
+
+    # Update the baord
+    # def update(self, cell, player):
+
+def welcome_msg():
+    print("Hello, Smack Talk Toe, user!")
+
+while True:
+    welcome_msg()
+    break
