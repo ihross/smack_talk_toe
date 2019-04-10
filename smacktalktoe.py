2d85bd7fd2fbe7f36b3434e40377f7ab4217d174
Ian Ross <ianhowesross@gmail.com>
2019-04-10 16:56:53 -0400

    Psuedo coding

--- smacktalktoe.py
+++ smacktalktoe.py
@@ -1,7 +1,14 @@
 print("Hello, Smack Talk Toe, user!")
 
 """
-Let us do this...
-Within here fit the psuedo coding comments. 
+--Psuedo code--
 
+Initial Tasks:
+-Present information
+-Set up board
+-Display board
+-Two-player functionality
+-Win/lose logic
+-Allow game to be played again
 """
+
